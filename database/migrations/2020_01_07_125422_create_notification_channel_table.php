b04e23f468a901549e5d851f69a6fb85f2364f49
iulia.tugui@ivfuture.uk <iulia.tugui@ivfuture.uk>
2020-01-15 11:46:54 +0200

    changed the table name from notification_type to notification_channel

--- database/migrations/2020_01_07_125422_create_notification_channel_table.php
+++ database/migrations/2020_01_07_125422_create_notification_channel_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotificationTypeTable extends Migration
+class CreateNotificationChannelTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNotificationTypeTable extends Migration
      */
     public function up()
     {
-        Schema::create('notification_type', function (Blueprint $table) {
+        Schema::create('notification_channel', function (Blueprint $table) {
             $table->increments('id');
             $table->string('label');
             $table->string('icon');
@@ -29,6 +29,6 @@ class CreateNotificationTypeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notification_type');
+        Schema::dropIfExists('notification_channel');
     }
 }
